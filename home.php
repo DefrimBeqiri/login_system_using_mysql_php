a3b65f4a2a5bb07d74413f1d5b7a826170ebb4f0
DefrimBeqiri <162150021+DefrimBeqiri@users.noreply.github.com>
2024-03-13 11:14:06 +0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+if (!isset($_SESSION['user_id'])) {
+    header("Location: login.php");
+}
+?>
+
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+    <h2>Welcome, <?php echo $_SESSION['username']; ?>!</h2>
+    <a href="logout.php">Logout</a>
+</body>
+</html>
